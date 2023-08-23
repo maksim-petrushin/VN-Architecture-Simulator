172cedd9c2c7ac69f39c5c4910d39b33e14c4288
Maksim Petrushin <petrushin.maksim001@gmail.com>
2023-08-23 17:49:21 -0400

    fixed bugs

--- proj.c
+++ proj.c
@@ -1,12 +1,18 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <SDL.h>
 #include "./constants.h"
 
-int game_is_running = 0;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+///////////////////////////////////////////////////////////////////////////////
+// Global variables
+///////////////////////////////////////////////////////////////////////////////
+int game_is_running = false;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+
+///////////////////////////////////////////////////////////////////////////////
+// Declare two game objects for the ball and the paddle
+///////////////////////////////////////////////////////////////////////////////
 struct game_object {
     float x;
     float y;
@@ -16,99 +22,117 @@ struct game_object {
     float vel_y;
 } ball, paddle;
 
-
-int initialize_window(){
-   if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+///////////////////////////////////////////////////////////////////////////////
+// Function to initialize our SDL window
+///////////////////////////////////////////////////////////////////////////////
+int initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL.\n");
-        return 0;
+        return false;
     }
     window = SDL_CreateWindow(
-        "Maxat",
+        "A simple game loop using C & SDL",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH,
         WINDOW_WIDTH,
         0
     );
-    if(!window){
-        fprintf(stderr, "Error in window\n");
-        return 0;
+    if (!window) {
+        fprintf(stderr, "Error creating SDL Window.\n");
+        return false;
     }
-
     renderer = SDL_CreateRenderer(window, -1, 0);
-    
-    if(!renderer){
-        return 0;
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL Renderer.\n");
+        return false;
     }
-    return 1; 
+    return true;
 }
 
-void process_input(void){
+///////////////////////////////////////////////////////////////////////////////
+// Function to poll SDL events and process keyboard input
+///////////////////////////////////////////////////////////////////////////////
+void process_input(void) {
     SDL_Event event;
-    while(SDL_PollEvent(&event)){
-        switch(event.type){
-        case SDL_QUIT:
-        game_is_running = 0;
-        break;
-        case SDL_KEYDOWN:
-        if(event.key.keysym.sym == SDLK_ESCAPE){
-game_is_running = 0;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                game_is_running = false;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    game_is_running = false;
+                }
+                break;
         }
-        
-        break;
     }
 }
-    }
 
- void setup(void){
-    ball.x = 20;
+///////////////////////////////////////////////////////////////////////////////
+// Setup function that runs once at the beginning of our program
+///////////////////////////////////////////////////////////////////////////////
+void setup(void) {
+    // Initialize the ball object moving down at a constant velocity
+    ball.x = 10;
     ball.y = 20;
-    ball.height = 15;
-    ball.height = 15;
-    //todo
-}   
-
-void update(void){
-    //todo
+    ball.width = 20;
+    ball.height = 20;
+    ball.vel_x = 180;
+    ball.vel_y = 140;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Update function with a fixed time step
+///////////////////////////////////////////////////////////////////////////////
+void update(void) {
+   
+}
 
-void render(void){
+///////////////////////////////////////////////////////////////////////////////
+// Render function to draw game objects in the SDL window
+///////////////////////////////////////////////////////////////////////////////
+void render(void) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-   //Draw a rectangle
-   SDL_Rect ball_rect = {
+
+    // Draw a rectangle for the ball object
+    SDL_Rect ball_rect = {
         (int)ball.x,
         (int)ball.y,
         (int)ball.width,
         (int)ball.height
     };
-    //fill the rectangle
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); 
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderFillRect(renderer, &ball_rect);
-    //swapping the buffers
-    SDL_RenderPresent(renderer); 
-    //todo
+
+    SDL_RenderPresent(renderer);
 }
 
-
-
-void destroy_window(void){
+///////////////////////////////////////////////////////////////////////////////
+// Function to destroy SDL window and renderer
+///////////////////////////////////////////////////////////////////////////////
+void destroy_window(void) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
 }
-int main (int argc, char *argv[]){
+
+///////////////////////////////////////////////////////////////////////////////
+// Main function
+///////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* args[]) {
     game_is_running = initialize_window();
 
     setup();
-    while(game_is_running){
+
+    while (game_is_running) {
         process_input();
         update();
         render();
     }
 
     destroy_window();
+
     return 0;
 }
